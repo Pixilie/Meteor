5688cc1a252e27529bfeec8e0d17154d2792ba4a
Pixilie <kristen.couty@gmail.com>
2021-01-25 21:44:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,69 @@
 from tkinter import *
 import requests
-import json    
+import json
+import time
 
-city = "Paris"
 API = "33f784258bbf921ad48a2b9b3d06d4c6"
 units = "metric"
 lang = "fr"
 
+
 def meteo_window():
     #Creation fenetre
     meteo = Tk()
-
+    city = entry.get()
+    
     #Paramètre de la fenêtre
-    meteo.title("Meteor")
+    meteo.title("Metéo de la ville de " + city)
     meteo.geometry("740x510")
     meteo.minsize(740, 510)
     window.minsize(740, 510)
     meteo.iconbitmap("meteor.ico")
     meteo.config(background='#00C5FE')
+    return meteo
+
 
 def search():
-    city=entry
-    url="http://api.openweathermap.org/data/2.5/weather?q=" + entry + "&appid=33f784258bbf921ad48a2b9b3d06d4c6&units=metric&lang=fr"
-    response = resquets.get(url)
-    weather= (response.json())
-    meteo_window()
-    label_title = Label(window, text=print(weather), font=("Arial", 40), bg='#00C5FE', fg='white')
-    label_title.pack()
+    city = entry.get()
+    url="http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&lang=fr&appid=33f784258bbf921ad48a2b9b3d06d4c6"
+    response = requests.get(url)
+    response_json = (response.json())
+    
+    main_info = response_json.get('main')
+    wind_info = response_json.get('wind')
+    clouds_info = response_json.get('clouds')
+    sys_info = response_json.get('sys')
+    weather_info = response_json.get('weather')
+
+    temp_max = main_info.get('temp_max')
+    temp_min = main_info.get('temp_min')
+    temp = main_info.get('temp')
+    ressenti = main_info.get('feels_like')
+    pressure = main_info.get('pressure')
+    humidity = main_info.get('humidity')
+    wind_speed = wind_info.get('speed')
+    clouds = clouds_info.get('all')
+    country = sys_info.get('country')
+    sunrise = sys_info.get('sunrise')
+    sunrise_info = time.ctime(sunrise)
+    sunset = sys_info.get('sunset')
+    sunset_info = time.ctime(sunset)
+    timezone = sys_info.get('timezone')
+    name = sys_info.get('name')
+    date = response_json.get('dt')
+    date_info = time.ctime(date)
+    #description = weather_info.get()
+
+    print(reponse_json)
+
+    reponse = "Nom: " + city + "\n" + "Pays: " + str(country) + "\n" + "Date & Heure: " + date_info + "\n" + "Temps: " + "str(description)" + "\n" + "\n" + "\n" + "Température :" + str(temp) + " °C" + "\n" + "Température minimum: " + str(temp_min) + " °C"+ "\n" +  "Température maximum: " + str(temp_max) + " °C" + "\n" + "Température ressentie: " + str(ressenti) + " °C" + "\n" + "\n" + "Humidité: " + str(humidity) + " %" + "\n" + "Pression: " + str(pressure) + " pascal" + "\n" + "Vitesse du vent: " + str(wind_speed) + " km/h" + "\n" + "Pourcentage de nuage: " + str(clouds) + " %" + "\n" + "\n" + "Le soleil se couchera à: " + str(sunset_info) + "\n" + "Le soleil se lèvera à: " + str(sunrise_info) + "\n"
+
+    meteo = meteo_window()
+    titre = Label(meteo, text="Météo de " + city, font=("Arial", 30), bg='#00C5FE', fg='white')
+    titre.pack()
+    temps = Label(meteo, text=reponse, font=("Arial", 15), bg='#00C5FE', fg='white')
+    temps.pack()
+
 
 
 #Creation fenetre
