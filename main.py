25253b246a6f62e5ce0ecb9203e2616470f2a075
Pixilie <kristen.couty@gmail.com>
2021-01-30 14:20:21 +0100

    Touche final

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from tkinter import *
 import requests 
 import json
 import time
+import webbrowser
 
 API = "33f784258bbf921ad48a2b9b3d06d4c6"
-units = "metric"
-lang = "fr"
+
+
+def callback(event):
+    webbrowser.open_new("http://www.openweathermap.org")
 
 def meteo_window():
     #Creation fenetre
@@ -24,12 +27,11 @@ def meteo_window():
 
 def search():
     city = entry.get()
-    #unit = Setting_window()
     url="http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&lang=fr&appid=33f784258bbf921ad48a2b9b3d06d4c6"
     response = requests.get(url)
     response_json = (response.json())
     meteo = meteo_window()
-    
+
     main_info = response_json.get('main')
     wind_info = response_json.get('wind')
     clouds_info = response_json.get('clouds')
@@ -56,10 +58,8 @@ def search():
     description = weather_info[0].get('description')
     icon = weather_info[0].get('icon')
 
-    print(response_json)
-
-    reponse = "Nom: " + city + "\n" + "Pays: " + str(country) + "\n" + "Date & Heure: " + date_info + "\n" + "Temps: " + str(description) + "\n" + "\n" + "\n" + "Température :" + str(temp) + " °C" + "\n" + "Température minimum: " + str(temp_min) + " °C"+ "\n" +  "Température maximum: " + str(temp_max) + " °C" + "\n" + "Température ressentie: " + str(ressenti) + " °C" + "\n" + "\n" + "Humidité: " + str(humidity) + " %" + "\n" + "Pression: " + str(pressure) + " hectopascal" + "\n" + "Vitesse du vent: " + str(wind_speed) + " km/h" + "\n" + "Pourcentage de nuage: " + str(clouds) + " %" + "\n" + "\n" + "Le soleil se couchera à: " + str(sunset_info) + "\n" + "Le soleil se lèvera à: " + str(sunrise_info) + "\n"
-
+    reponse = "Nom: " + city + "\n" + "Pays: " + str(country) + "\n" + "Date & Heure: " + date_info + "\n" + "Temps: " + str(description) + "\n" + "\n" + "\n" + "Température :" + str(temp) + " °C" + "\n" + "Température minimum: " + str(temp_min) + " °C"+ "\n" +  "Température maximum: " + str(temp_max) + " °C" + "\n" + "Température ressentie: " + str(ressenti) + " °C" + "\n" + "\n" + "Humidité: " + str(humidity) + " %" + "\n" + "Pression: " + str(pressure) + " hpa" + "\n" + "Vitesse du vent: " + str(wind_speed) + " km/h" + "\n" + "Pourcentage de nuage: " + str(clouds) + " %" + "\n" + "\n" + "Le soleil se couchera à: " + str(sunset_info) + "\n" + "Le soleil s'est levé: " + str(sunrise_info) + "\n"
+        
     titre = Label(meteo, text="Météo de " + city, font=("Arial", 30), bg='#00C5FE', fg='white')
     titre.pack()
     temps = Label(meteo, text=reponse, font=("Arial", 15), bg='#00C5FE', fg='white')
@@ -97,7 +97,7 @@ label_subtitle = Label(frame, text="Votre temps par tous les temps", font=("Aria
 label_subtitle.pack()
 
 #champs d'écriture
-entry = Entry(frame, text="Insérer la ville ", font=("Arial", 20), bg='white', fg='black')
+entry = Entry(frame, text="Insérer la ville", font=("Arial", 20), bg='white', fg='black')
 entry.pack(pady=50)
 
 #bouton1
@@ -105,6 +105,7 @@ button = Button(frame, text="Chercher", font=("Arial", 20), bg='white', fg='blac
 button.pack()
 
 def Setting_window():
+
     #Creation fenetre
     setting = Tk()
 
@@ -113,7 +114,7 @@ def Setting_window():
     frame_setting.pack(expand=YES)
     
     #Paramètre de la fenêtre
-    setting.title("Paramètres")
+    setting.title("Crédits")
     setting.geometry("740x510")
     setting.minsize(740, 510)
     setting.maxsize(740, 510)
@@ -121,20 +122,19 @@ def Setting_window():
     setting.config(background='#00C5FE')
 
     #Textes
-    text_units = Label(frame_setting, text="Pour choisir votre unité mettez : \n - Metric : pour utiliser les degrés Celcius et les kilomètres \n - Imperial : pour utiliser les degrés Fahrenheit et les miles", font=("Arial", 15), bg='#00C5FE', fg='white')
+    text_units = Label(frame_setting, text="Cette application a été créée par Kristen Couty et Thomas Bruno. \n Elle a été créée à l'aide de l'API openweathermap.org", font=("Arial", 15), bg='#00C5FE', fg='white')
     text_units.pack()
 
-    #champs d'écriture
-    entry_units = Entry(frame_setting, text="Insérer la ville", font=("Arial", 20), bg='white', fg='black')
-    entry_units.pack()
-    unit = entry_units.get()
-    return unit
+    #liens
+    link = Label(frame_setting, text="OpenWeather Map", font=("Arial", 15), fg="blue", bg='#00C5FE', cursor="hand2")
+    link.pack()
+    link.bind("<Button-1>", callback)
     
 
 #bouton parametre
-#image_setting = PhotoImage(file='app_icon\setting_icon.png', width=50, height=50)
-#setting = Button(window, image=image_setting, bd=0, bg='#00C5FE', fg='black', relief=SUNKEN, command=Setting_window)
-#setting.place(x=5, y=5)
+image_setting = PhotoImage(file='app_icon\setting_icon.png', width=50, height=50)
+setting = Button(window, image=image_setting, bd=0, bg='#00C5FE', fg='black', relief=SUNKEN, command=Setting_window)
+setting.place(x=5, y=5)
 
 #enpacktage
 frame.pack(expand=YES)
